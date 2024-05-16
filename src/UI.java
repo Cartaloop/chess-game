692c367825cee10997181b7879cc8cda4b457fb9
Cartaloop <lucasrech00@gmail.com>
2024-05-15 22:20:49 -0300

    Instantiating UI class

--- src/UI.java
+++ src/UI.java
@@ -4,7 +4,7 @@ public class UI {
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
-            System.out.println((8 - i) + " ");
+            System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces[i].length; j++) {
                 printPiece(pieces[i][j]);
             }
